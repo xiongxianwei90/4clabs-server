2c96463674671b8b9469a26f0df246b94aea2eae
Zheng Li <13681320844@163.com>
2022-07-03 18:42:45 +0800

    fix: fix

--- api/nft/v1/typs.pb.validate.go
+++ api/nft/v1/typs.pb.validate.go
@@ -92,6 +92,8 @@ func (m *ComicWork) validate(all bool) error {
 
 	// no validation rules for Name
 
+	// no validation rules for MetadataJson
+
 	// no validation rules for MinterAddress
 
 	// no validation rules for CreatedAtTimestamp
